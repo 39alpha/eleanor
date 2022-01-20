9f8db6a6794a56cb8f7e9603bb6a9cc653071071
Cole Mathis <cole.mathis.ool@gmail.com>
2022-01-19 21:12:24 -0700

    Test for orders to results

--- test/test_integration.py
+++ test/test_integration.py
@@ -28,3 +28,19 @@ class TestCampaign_to_Helmsman(unittest.TestCase):
             order_num = eleanor.hanger.db_comms.get_order_number(this_conn)
             self.assertTrue(order_num == 1)
             # Need to clean this up by removing CSSO directory
+
+    def test_orders_to_results(self):
+        """
+        Confirm that the demo campaign CSS0 can be generated and converted into orders
+        """
+        demo_camp_file = "demo/CSS0.json"
+        with TemporaryDirectory() as root:
+            my_camp = eleanor.Campaign.from_json(demo_camp_file, '/path/to/db')
+            my_camp.create_env(dir=root, verbose=False)
+
+            eleanor.Navigator(my_camp)
+            with my_camp.working_directory():
+                this_conn = eleanor.hanger.db_comms.establish_database_connection(my_camp)
+            order_num = eleanor.hanger.db_comms.get_order_number(this_conn)
+            self.assertTrue(order_num == 1)
+            # Need to clean this up by removing CSSO directory

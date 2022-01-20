c11f2a989c098ff544ade9a093d1e767b1a52784
Cole Mathis <cole.mathis.ool@gmail.com>
2022-01-19 21:14:30 -0700

    Thats the actual test

--- test/test_integration.py
+++ test/test_integration.py
@@ -31,16 +31,10 @@ class TestCampaign_to_Helmsman(unittest.TestCase):
 
     def test_orders_to_results(self):
         """
-        Confirm that the demo campaign CSS0 can be generated and converted into orders
+        Confirm that the demo campaign CSS0 Orders can be read and submitted to sailors
         """
-        demo_camp_file = "demo/CSS0.json"
-        with TemporaryDirectory() as root:
-            my_camp = eleanor.Campaign.from_json(demo_camp_file, '/path/to/db')
-            my_camp.create_env(dir=root, verbose=False)
 
-            eleanor.Navigator(my_camp)
-            with my_camp.working_directory():
-                this_conn = eleanor.hanger.db_comms.establish_database_connection(my_camp)
-            order_num = eleanor.hanger.db_comms.get_order_number(this_conn)
-            self.assertTrue(order_num == 1)
-            # Need to clean this up by removing CSSO directory
+        from eleanor.helmsman import main
+        demo_camp_file = "demo/CSS0.json"
+        my_camp = eleanor.Campaign.from_json(demo_camp_file)
+        main(my_camp, 1)

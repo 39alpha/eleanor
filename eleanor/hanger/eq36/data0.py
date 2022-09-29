dae611ae41233ffb0227851ce8ab551159ce372f
Douglas G. Moore <doug@dglmoore.com>
2022-09-29 00:39:42 -0700

    Flesh out data0 verification

--- eleanor/hanger/eq36/data0.py
+++ eleanor/hanger/eq36/data0.py
@@ -1,4 +1,138 @@
-from typing import Iterator
+import numpy as np
+
+Integer = int | np.int64
+Float = float | np.float64
+Number = int | np.int64 | float | np.float64
+
+class Params(object):
+    def __init__(self,
+                 temperatures: np.ndarray,
+                 pressures: np.ndarray,
+                 debye_huckel_a: np.ndarray,
+                 debye_huckel_b: np.ndarray,
+                 bdot: np.ndarray,
+                 cco2: np.ndarray,
+                 ehlogk: np.ndarray):
+
+        self.temperatures = temperatures
+        self.pressures = pressures
+        self.debye_huckel_a = debye_huckel_a
+        self.debye_huckel_b = debye_huckel_b
+        self.bdot = bdot
+        self.cco2 = cco2
+        self.ehlogk = ehlogk
+
+class BDotSpecies(object):
+    def __init__(self, name: str, azer0: Float, neutral_ion_type: Integer):
+        self.name = name
+        self.azer0 = azer0
+        self.neutral_ion_type = neutral_ion_type
+
+class Dissociation(object):
+    def __init__(self, substrates: dict[str, Integer], products: dict[str, Integer]):
+        self.substrates = substrates
+        self.products = products
+
+    def __repr__(self) -> str:
+        return f'Dissociation(substrates={repr(self.substrates)}, products={repr(self.products)})'
+
+    def __str__(self) -> str:
+        substrates = ' + '.join(f'{count} {name}' for name, count in self.substrates.items())
+        products = ' + '.join(f'{count} {name}' for name, count in self.products.items())
+        return f'{substrates} ⇌ {products}'
+
+class Volume(object):
+    def __init__(self, value: Number, unit: str):
+        self.value = value
+        self.unit = unit
+
+    def __repr__(self) -> str:
+        return f"Volume(value={repr(self.value)}, unit={repr(self.unit)})"
+
+    def __str__(self) -> str:
+        return f'{self.value}{self.unit}'
+
+class Species(object):
+    def __init__(self, name: str, composition: dict[str, Integer]):
+        self.name = name
+        self.composition = composition
+
+class BasicSpecies(Species):
+    def __init__(self, charge: Integer, volume: Volume | None = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.charge = charge
+        self.volume = volume
+
+class ComplexSpecies(BasicSpecies):
+    def __init__(self, dissociation: Dissociation, logk: np.ndarray, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dissociation = dissociation
+        self.logk = logk
+
+class BasisSpecies(BasicSpecies):
+    def __repr__(self) -> str:
+        return f'BasisSpecies(name={repr(self.name)}, charge={repr(self.charge)}, volume={repr(self.volume)}, composition={repr(self.composition)})'
+
+    def __str__(self) -> str:
+        return f'<BasisSpecies {self.name}>'
+
+class AuxiliaryBasisSpecies(ComplexSpecies):
+    def __repr__(self) -> str:
+        return f'AuxiliaryBasisSpecies(name={repr(self.name)}, charge={repr(self.charge)}, volume={repr(self.volume)}, composition={repr(self.composition)}, decomposition={repr(self.dissociation)}, logk={repr(self.logk)})'
+
+    def __str__(self) -> str:
+        return f'<AuxiliaryBasisSpecies {self.name}>'
+
+class AqueousSpecies(ComplexSpecies):
+    def __repr__(self) -> str:
+        return f'AqueousSpecies(name={repr(self.name)}, charge={repr(self.charge)}, volume={repr(self.volume)}, composition={repr(self.composition)}, decomposition={repr(self.dissociation)}, logk={repr(self.logk)})'
+
+    def __str__(self) -> str:
+        return f'<AqueousSpecies {self.name}>'
+
+class Solid(ComplexSpecies):
+    def __repr__(self) -> str:
+        return f'Solid(name={repr(self.name)}, charge={repr(self.charge)}, volume={repr(self.volume)}, composition={repr(self.composition)}, decomposition={repr(self.dissociation)}, logk={repr(self.logk)})'
+
+    def __str__(self) -> str:
+        return f'<Solid {self.name}>'
+
+class Liquid(ComplexSpecies):
+    def __repr__(self) -> str:
+        return f'Liquid(name={repr(self.name)}, charge={repr(self.charge)}, volume={repr(self.volume)}, composition={repr(self.composition)}, decomposition={repr(self.dissociation)}, logk={repr(self.logk)})'
+
+    def __str__(self) -> str:
+        return f'<Liquid {self.name}>'
+
+class Gas(ComplexSpecies):
+    def __repr__(self) -> str:
+        return f'Gas(name={repr(self.name)}, charge={repr(self.charge)}, volume={repr(self.volume)}, composition={repr(self.composition)}, decomposition={repr(self.dissociation)}, logk={repr(self.logk)})'
+
+    def __str__(self) -> str:
+        return f'<Gas {self.name}>'
+
+class SolidSolutionModel(object):
+    def __init__(self, type: Integer, params: np.ndarray):
+        self.type = type
+        self.params = params
+
+    def __repr__(self) -> str:
+        return f'SolidSolutionModel(type={repr(self.type)}, params={repr(self.params)})'
+
+    def __str__(self) -> str:
+        return f'<SolidSolutionModel {self.type} {self.params}>'
+
+class SolidSolution(Species):
+    def __init__(self, model: SolidSolutionModel, site_params: np.ndarray, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.site_params = site_params
+
+    def __repr__(self) -> str:
+        return f'SolidSolutionModel(name={repr(self.name)}, composition={repr(self.composition)}, model={repr(self.model)}, site_params={repr(self.site_params)})'
+
+    def __str__(self) -> str:
+        return f'<SolidSolution {self.name}>'
 
 class Data0(object):
     @staticmethod
@@ -7,9 +141,22 @@ class Data0(object):
         data0, _ = parse_data0(fname, *args, **kwargs)
         return data0
 
-    def __init__(self, fname=None, magic=None, header=None, params=None, bdot=None, elements=None,
-                 basis_species=None, auxiliary_basis_species=None, aqueous_species=None,
-                 solids=None, liquids=None, gases=None, solid_solutions=None, references=None):
+    def __init__(self,
+                 magic: str,
+                 header: str,
+                 params: Params,
+                 bdot: dict[str, BDotSpecies],
+                 elements: dict[str, Float],
+                 basis_species: dict[str, BasisSpecies],
+                 auxiliary_basis_species: dict[str, AuxiliaryBasisSpecies],
+                 aqueous_species: dict[str, AqueousSpecies],
+                 solids: dict[str, Solid],
+                 liquids: dict[str, Liquid],
+                 gases: dict[str, Gas],
+                 solid_solutions: dict[str, SolidSolution],
+                 references: str,
+                 fname: str | None = None):
+
         self.fname = fname
         self.magic = magic
         self.header = header
@@ -24,82 +171,63 @@ class Data0(object):
         self.gases = gases
         self.solid_solutions = solid_solutions
         self.references = references
+        self.__species = set[str]()
 
-class Params(object):
-    def __init__(self):
-        self.temperatures = None
-        self.pressures = None
-        self.debye_huckel_a = None
-        self.debye_huckel_b = None
-        self.bdot = None
-        self.cco2 = None
-        self.ehlogk = None
+    def reify(self):
+        self.__species = set(self.elements.keys())
+        self.__species.update(self.basis_species.keys())
+        self.__species.update(self.auxiliary_basis_species.keys())
+        self.__species.update(self.aqueous_species.keys())
+        self.__species.update(self.solids.keys())
+        self.__species.update(self.liquids.keys())
+        self.__species.update(self.gases.keys())
+        self.__species.update(self.solid_solutions.keys())
 
-class BDotSpecies(object):
-    def __init__(self):
-        self.name = None
-        self.azer0 = None
-        self.neutral_ion_type = None
+    def verify(self):
+        self.__verify_params()
 
-class Dissociation(object):
-    def __init__(self, substrates=None, products=None):
-        self.substrates = substrates if substrates is not None else dict()
-        self.products = products if products is not None else dict()
+        self.__verify_basic_species(self.basis_species)
+        self.__verify_complex_species(self.auxiliary_basis_species)
+        self.__verify_complex_species(self.aqueous_species)
+        self.__verify_complex_species(self.solids)
+        self.__verify_complex_species(self.liquids)
+        self.__verify_complex_species(self.gases)
+        self.__verify_solid_solutions(self.solid_solutions)
 
-    def __repr__(self) -> str:
-        return f'Dissociation(substrats={self.substrates}, products={self.products})'
+    def __verify_params(self):
+        num_grid_points = len(self.params.temperatures)
 
-    def __str__(self) -> str:
-        substrates = ' + '.join(f'{count} {name}' for name, count in self.substrates.items())
-        products = ' + '.join(f'{count} {name}' for name, count in self.products.items())
-        return f'{substrates} ⇌ {products}'
+        n = len(self.params.pressures)
+        if n != num_grid_points:
+            raise Exception(f'number of pressures in params section ({n}) differs from the number of temperatures ({num_grid_points})')
 
-class Species(object):
-    def __init__(self):
-        self.name = None
-        self.composition = None
+        n = len(self.params.ehlogk)
+        if n != num_grid_points:
+            raise Exception(f'number of ehlogk values in params section ({n}) differs from the number of temperatures ({num_grid_points})')
 
-class BasicSpecies(Species):
-    def __init__(self):
-        self.charge = None
-        self.volume = None
+    def __verify_basic_species(self, species):
+        for s in species.values():
+            for element in s.composition.keys():
+                if element not in self.elements:
+                    raise Exception(f'{species} has an element {element} that does not appear in the data0 element list')
 
-class ComplexSpecies(BasicSpecies):
-    def __init__(self):
-        super().__init__()
-        self.dissociation = None
-        self.logk = None
+    def __verify_complex_species(self, species):
+        self.__verify_basic_species(species)
 
-class BasisSpecies(BasicSpecies):
-    pass
+        for s in species.values():
+            for substrate in s.dissociation.substrates.keys():
+                if substrate not in self.__species:
+                    raise Exception(f'{species} has a substrate ({substrate}) in its dissocation reaction ({species.dissociation}) that does not appear in the elements or any species sections')
 
-class AuxiliaryBasisSpecies(ComplexSpecies):
-    pass
+            for product in s.dissociation.products.keys():
+                if product not in self.__species:
+                    raise Exception(f'{species} has a product ({product}) in its dissocation reaction ({species.dissociation}) that does not appear in the elements or any species sections')
 
-class AqueousSpecies(ComplexSpecies):
-    pass
+            if len(s.logk) != len(self.params.temperatures):
+                raise Exception(f'{species} has a different number of log(K) values than the number of values in the data0 temperature grid')
 
-class Solid(ComplexSpecies):
-    pass
-
-class Liquid(ComplexSpecies):
-    pass
-
-class Gas(ComplexSpecies):
-    pass
-
-class SolidSolution(Species):
-    def __init__(self):
-        super().__init__()
-        self.model = None
-        self.site_params = None
-
-class SolidSolutionModel(object):
-    def __init__(self):
-        self.type = None
-        self.params = None
-
-class Volume(object):
-    def __init__(self):
-        self.value = None
-        self.unit = None
+    def __verify_solid_solutions(self, solid_solutions):
+        for solid_solution in solid_solutions.values():
+            for solid in solid_solution.composition.keys():
+                if solid not in self.solids:
+                    raise Exception(f'{solid_solutions} has an solid ({solid}) that does not appear in the data0 solids section')
--- eleanor/hanger/eq36/parsers/Data0Builder.py
+++ eleanor/hanger/eq36/parsers/Data0Builder.py
@@ -33,6 +33,9 @@ class Data0Builder(Data0Listener):
                          solid_solutions=ctx.solidSolutionsSection().data,
                          references=ctx.referenceSection().data)
 
+        ctx.data.reify()
+        ctx.data.verify()
+
         self.data = ctx.data
 
     def exitMagic(self, ctx: Data0Parser.MagicContext):
@@ -42,14 +45,13 @@ class Data0Builder(Data0Listener):
         ctx.data = ctx.getText()
 
     def exitParamsSection(self, ctx: Data0Parser.ParamsSectionContext):
-        ctx.data = Params()
-        ctx.data.temperatures = ctx.temperatures().data
-        ctx.data.pressures = ctx.pressures().data
-        ctx.data.debye_huckel_a = ctx.debyeHuckelA().data
-        ctx.data.debye_huckel_b = ctx.debyeHuckelB().data
-        ctx.data.bdot = ctx.bdot().data
-        ctx.data.cco2 = ctx.cco2().data
-        ctx.data.ehlogk = ctx.eHLogK().data
+        ctx.data = Params(temperatures=ctx.temperatures().data,
+                          pressures=ctx.pressures().data,
+                          debye_huckel_a=ctx.debyeHuckelA().data,
+                          debye_huckel_b=ctx.debyeHuckelB().data,
+                          bdot=ctx.bdot().data,
+                          cco2=ctx.cco2().data,
+                          ehlogk=ctx.eHLogK().data)
 
     def exitTemperatures(self, ctx: Data0Parser.TemperaturesContext):
         min, max, *rest = ctx.numberLine().data
@@ -83,15 +85,16 @@ class Data0Builder(Data0Listener):
         ctx.data = ctx.numberGrid().data
 
     def exitBdotSpeciesSection(self, ctx: Data0Parser.BdotSpeciesSectionContext):
-        ctx.data = [bdot.data for bdot in ctx.bdotSpecies()]
+        ctx.data = {bdot.data.name: bdot.data for bdot in ctx.bdotSpecies()}
 
     def exitBdotSpecies(self, ctx: Data0Parser.BdotSpeciesContext):
-        ctx.data = BDotSpecies()
-        ctx.data.name = ctx.WORD().getText()
+        name = ctx.WORD().getText()
 
         azer0, neutral_ion_type = ctx.NUMBER()
-        ctx.data.azer0 = np.float64(azer0.getText())
-        ctx.data.neutral_ion_type = np.int64(neutral_ion_type.getText())
+        azer0 = np.float64(azer0.getText())
+        neutral_ion_type = np.int64(neutral_ion_type.getText())
+
+        ctx.data = BDotSpecies(name=name, azer0=azer0, neutral_ion_type=neutral_ion_type)
 
     def exitElementsSection(self, ctx: Data0Parser.ElementsSectionContext):
         ctx.data = {name: weight for name, weight in map(lambda c: c.data, ctx.element())}
@@ -102,7 +105,7 @@ class Data0Builder(Data0Listener):
         ctx.data = (name, weight)
 
     def exitBasisSpeciesSection(self, ctx: Data0Parser.BasisSpeciesSectionContext):
-        ctx.data = [species.data for species in ctx.basisSpecies()]
+        ctx.data = {species.data.name: species.data for species in ctx.basisSpecies()}
 
     def exitChargeLine(self, ctx: Data0Parser.ChargeLineContext):
         ctx.data = np.float64(ctx.NUMBER().getText())
@@ -129,7 +132,7 @@ class Data0Builder(Data0Listener):
 
     def exitAuxiliaryBasisSpeciesSection(self,
                                          ctx: Data0Parser.AuxiliaryBasisSpeciesSectionContext):
-        ctx.data = [species.data for species in ctx.auxiliaryBasisSpecies()]
+        ctx.data = {species.data.name: species.data for species in ctx.auxiliaryBasisSpecies()}
 
     def exitDissociation(self, ctx: Data0Parser.DissociationContext):
         substrates = dict()
@@ -166,24 +169,24 @@ class Data0Builder(Data0Listener):
         ctx.data = np.asarray([np.float64(n.getText()) for n in ctx.NUMBER()])
 
     def exitAqueousSpeciesSection(self, ctx: Data0Parser.AqueousSpeciesSectionContext):
-        ctx.data = [species.data for species in ctx.aqueousSpecies()]
+        ctx.data = {species.data.name: species.data for species in ctx.aqueousSpecies()}
 
     def exitSolidsSection(self, ctx: Data0Parser.SolidsSectionContext):
-        ctx.data = [solid.data for solid in ctx.solid()]
+        ctx.data = {solid.data.name: solid.data for solid in ctx.solid()}
 
     def exitLiquidsSection(self, ctx: Data0Parser.LiquidsSectionContext):
-        ctx.data = [liquid.data for liquid in ctx.liquid()]
+        ctx.data = {liquid.data.name: liquid.data for liquid in ctx.liquid()}
 
     def exitGasesSection(self, ctx: Data0Parser.GasesSectionContext):
-        ctx.data = [gas.data for gas in ctx.gas()]
+        ctx.data = {gas.data.name: gas.data for gas in ctx.gas()}
 
     def exitVolumeLine(self, ctx: Data0Parser.VolumeLineContext):
-        ctx.data = Volume()
-        ctx.value = np.float64(ctx.NUMBER().getText())
-        ctx.unit = ctx.restOfLine().getText()
+        ctx.data = Volume(value=np.float64(ctx.NUMBER().getText()),
+                          unit=ctx.restOfLine().getText())
 
     def exitSolidSolutionsSection(self, ctx: Data0Parser.SolidSolutionsSectionContext):
-        ctx.data = [solid_solution.data for solid_solution in ctx.solidSolution()]
+        ctx.data = {solid_solution.data.name: solid_solution.data
+                    for solid_solution in ctx.solidSolution()}
 
     def exitComponents(self, ctx: Data0Parser.ComponentsContext):
         ctx.data = dict()
@@ -198,9 +201,8 @@ class Data0Builder(Data0Listener):
             raise Exception(msg)
 
     def exitModelSpec(self, ctx: Data0Parser.ModelSpecContext):
-        ctx.data = SolidSolutionModel()
-        ctx.data.type = ctx.modelType().data
-        ctx.data.params = ctx.modelParams().data
+        ctx.data = SolidSolutionModel(type=ctx.modelType().data,
+                                      params=ctx.modelParams().data)
 
     def exitModelType(self, ctx: Data0Parser.ModelTypeContext):
         ctx.data = np.int64(ctx.NUMBER().getText())
@@ -239,142 +241,182 @@ class Data0Builder(Data0Listener):
         ctx.data = ctx.getText()
 
     def exitBasisSpecies(self, ctx: Data0Parser.BasisSpeciesContext):
-        ctx.data = BasisSpecies()
-        ctx.data.name = ctx.WORD().getText()
+        name = ctx.WORD().getText()
 
         charges = ctx.chargeLine()
         if isinstance(charges, Data0Parser.ChargeLineContext):
-            ctx.data.charge = charges.data
+            charge = charges.data
         elif len(charges) == 1:
-            ctx.data.charge = charges[0].data
+            charge = charges[0].data
         elif len(charges) > 1:
             raise Exception(f'expected at most 1 charge line, got {len(charges)}')
 
-        ctx.data.composition = ctx.composition().data
+        composition = ctx.composition().data
+
+        ctx.data = BasisSpecies(name=name, charge=charge, composition=composition)
 
     def exitAuxiliaryBasisSpecies(self, ctx: Data0Parser.AuxiliaryBasisSpeciesContext):
-        ctx.data = AuxiliaryBasisSpecies()
-        ctx.data.name = ctx.WORD().getText()
+        name = ctx.WORD().getText()
 
+        charge = None
         charges = ctx.chargeLine()
         if isinstance(charges, Data0Parser.ChargeLineContext):
-            ctx.data.charge = charges.data
+            charge = charges.data
         elif len(charges) == 1:
-            ctx.data.charge = charges[0].data
+            charge = charges[0].data
         elif len(charges) > 1:
             raise Exception(f'expected at most 1 charge line, got {len(charges)}')
 
+        volume = None
         volumes = ctx.volumeLine()
         if isinstance(volumes, Data0Parser.VolumeLineContext):
-            ctx.data.volume = volumes.data
+            volume = volumes.data
         elif len(volumes) == 1:
-            ctx.data.volume = volumes[0].data
+            volume = volumes[0].data
         elif len(volumes) > 1:
             raise Exception(f'expected at most 1 volume line, got {len(volumes)}')
 
-        ctx.data.composition = ctx.composition().data
-        ctx.data.dissociation = ctx.dissociation().data
-        ctx.data.logk = ctx.numberGrid().data
+        composition = ctx.composition().data
+        dissociation = ctx.dissociation().data
+        logk = ctx.numberGrid().data
+
+        ctx.data = AuxiliaryBasisSpecies(name=name,
+                                         charge=charge,
+                                         volume=volume,
+                                         composition=composition,
+                                         dissociation=dissociation,
+                                         logk=logk)
 
     def exitAqueousSpecies(self, ctx: Data0Parser.AqueousSpeciesContext):
-        ctx.data = AqueousSpecies()
-        ctx.data.name = ctx.WORD().getText()
+        name = ctx.WORD().getText()
 
+        charge = None
         charges = ctx.chargeLine()
         if isinstance(charges, Data0Parser.ChargeLineContext):
-            ctx.data.charge = charges.data
+            charge = charges.data
         elif len(charges) == 1:
-            ctx.data.charge = charges[0].data
+            charge = charges[0].data
         elif len(charges) > 1:
             raise Exception(f'expected at most 1 charge line, got {len(charges)}')
 
+        volume = None
         volumes = ctx.volumeLine()
         if isinstance(volumes, Data0Parser.VolumeLineContext):
-            ctx.data.volume = volumes.data
+            volume = volumes.data
         elif len(volumes) == 1:
-            ctx.data.volume = volumes[0].data
+            volume = volumes[0].data
         elif len(volumes) > 1:
             raise Exception(f'expected at most 1 volume line, got {len(volumes)}')
 
-        ctx.data.composition = ctx.composition().data
-        ctx.data.dissociation = ctx.dissociation().data
-        ctx.data.logk = ctx.numberGrid().data
+        composition = ctx.composition().data
+        dissociation = ctx.dissociation().data
+        logk = ctx.numberGrid().data
+
+        ctx.data = AqueousSpecies(name=name,
+                                  charge=charge,
+                                  volume=volume,
+                                  composition=composition,
+                                  dissociation=dissociation,
+                                  logk=logk)
 
     def exitSolid(self, ctx: Data0Parser.SolidContext):
-        ctx.data = Solid()
-        ctx.data.name = ctx.WORD().getText()
+        name = ctx.WORD().getText()
 
+        charge = None
         charges = ctx.chargeLine()
         if isinstance(charges, Data0Parser.ChargeLineContext):
-            ctx.data.charge = charges.data
+            charge = charges.data
         elif len(charges) == 1:
-            ctx.data.charge = charges[0].data
+            charge = charges[0].data
         elif len(charges) > 1:
             raise Exception(f'expected at most 1 charge line, got {len(charges)}')
 
+        volume = None
         volumes = ctx.volumeLine()
         if isinstance(volumes, Data0Parser.VolumeLineContext):
-            ctx.data.volume = volumes.data
+            volume = volumes.data
         elif len(volumes) == 1:
-            ctx.data.volume = volumes[0].data
+            volume = volumes[0].data
         elif len(volumes) > 1:
             raise Exception(f'expected at most 1 volume line, got {len(volumes)}')
 
-        ctx.data.composition = ctx.composition().data
-        ctx.data.dissociation = ctx.dissociation().data
-        ctx.data.logk = ctx.numberGrid().data
+        composition = ctx.composition().data
+        dissociation = ctx.dissociation().data
+        logk = ctx.numberGrid().data
+
+        ctx.data = Solid(name=name,
+                         charge=charge,
+                         volume=volume,
+                         composition=composition,
+                         dissociation=dissociation,
+                         logk=logk)
 
     def exitLiquid(self, ctx: Data0Parser.LiquidContext):
-        ctx.data = Liquid()
-        ctx.data.name = ctx.WORD().getText()
+        name = ctx.WORD().getText()
 
+        charge = None
         charges = ctx.chargeLine()
         if isinstance(charges, Data0Parser.ChargeLineContext):
-            ctx.data.charge = charges.data
+            charge = charges.data
         elif len(charges) == 1:
-            ctx.data.charge = charges[0].data
+            charge = charges[0].data
         elif len(charges) > 1:
             raise Exception(f'expected at most 1 charge line, got {len(charges)}')
 
+        volume = None
         volumes = ctx.volumeLine()
         if isinstance(volumes, Data0Parser.VolumeLineContext):
-            ctx.data.volume = volumes.data
+            volume = volumes.data
         elif len(volumes) == 1:
-            ctx.data.volume = volumes[0].data
+            volume = volumes[0].data
         elif len(volumes) > 1:
             raise Exception(f'expected at most 1 volume line, got {len(volumes)}')
 
-        ctx.data.composition = ctx.composition().data
-        ctx.data.dissociation = ctx.dissociation().data
-        ctx.data.logk = ctx.numberGrid().data
+        composition = ctx.composition().data
+        dissociation = ctx.dissociation().data
+        logk = ctx.numberGrid().data
+
+        ctx.data = Liquid(name=name,
+                          charge=charge,
+                          volume=volume,
+                          composition=composition,
+                          dissociation=dissociation,
+                          logk=logk)
 
     def exitGas(self, ctx: Data0Parser.GasContext):
-        ctx.data = Gas()
-        ctx.data.name = ctx.WORD().getText()
+        name = ctx.WORD().getText()
 
+        charge = None
         charges = ctx.chargeLine()
         if isinstance(charges, Data0Parser.ChargeLineContext):
-            ctx.data.charge = charges.data
+            charge = charges.data
         elif len(charges) == 1:
-            ctx.data.charge = charges[0].data
+            charge = charges[0].data
         elif len(charges) > 1:
             raise Exception(f'expected at most 1 charge line, got {len(charges)}')
 
+        volume = None
         volumes = ctx.volumeLine()
         if isinstance(volumes, Data0Parser.VolumeLineContext):
-            ctx.data.volume = volumes.data
+            volume = volumes.data
         elif len(volumes) == 1:
-            ctx.data.volume = volumes[0].data
+            volume = volumes[0].data
         elif len(volumes) > 1:
             raise Exception(f'expected at most 1 volume line, got {len(volumes)}')
 
-        ctx.data.composition = ctx.composition().data
-        ctx.data.dissociation = ctx.dissociation().data
-        ctx.data.logk = ctx.numberGrid().data
+        composition = ctx.composition().data
+        dissociation = ctx.dissociation().data
+        logk = ctx.numberGrid().data
+
+        ctx.data = Gas(name=name,
+                       charge=charge,
+                       volume=volume,
+                       composition=composition,
+                       dissociation=dissociation,
+                       logk=logk)
 
     def exitSolidSolution(self, ctx: Data0Parser.SolidSolutionContext):
-        ctx.data = SolidSolution()
-        ctx.data.name = ctx.WORD().getText()
-        ctx.data.composition = ctx.components().data
-        ctx.data.model = ctx.modelSpec().data
-        ctx.data.site_params = ctx.siteParams().data
+        ctx.data = SolidSolution(name=ctx.WORD().getText(),
+                                 composition=ctx.components().data,
+                                 model=ctx.modelSpec().data,
+                                 site_params=ctx.siteParams().data)

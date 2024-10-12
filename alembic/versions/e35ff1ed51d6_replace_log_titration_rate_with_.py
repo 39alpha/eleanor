fa34fee9ff035cfa88da9015a4b17b49584db2d6
Douglas G. Moore <doug@39alpharesearch.org>
2024-10-12 13:14:37 -0700

    Handle existing columns in migration

--- alembic/versions/e35ff1ed51d6_replace_log_titration_rate_with_.py
+++ alembic/versions/e35ff1ed51d6_replace_log_titration_rate_with_.py
@@ -17,11 +17,20 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
+def column_exists(table_name, column_name):
+    bind = op.get_context().bind
+    insp = sa.inspect(bind)
+    columns = insp.get_columns(table_name)
+    return any(c["name"] == column_name for c in columns)
+
+
 def upgrade() -> None:
     for reactant_type in ['mineral', 'gas', 'element', 'special']:
-        op.alter_column(f'{reactant_type}_reactants', 'log_titration_rate', new_column_name='titration_rate')
+        if column_exists(f'{reactant_type}_reactants', 'log_titration_rate'):
+            op.alter_column(f'{reactant_type}_reactants', 'log_titration_rate', new_column_name='titration_rate')
 
 
 def downgrade() -> None:
     for reactant_type in ['mineral', 'gas', 'element', 'special']:
-        op.alter_column(f'{reactant_type}_reactants', 'titration_rate', new_column_name='log_titration_rate')
+        if column_exists(f'{reactant_type}_reactants', 'titration_rate'):
+            op.alter_column(f'{reactant_type}_reactants', 'titration_rate', new_column_name='log_titration_rate')
